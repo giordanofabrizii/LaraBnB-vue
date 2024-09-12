53586ef2fc0be99f768b683f34fcc0131826d4f4
Piccio <lorenzo.hl.picchi@gmail.com>
2024-09-12 15:18:47 +0200

    added a botton to the search page

--- src/pages/C2.vue
+++ src/pages/C2.vue
@@ -3,20 +3,27 @@ export default {
     data() {
         return {
             sponsoredApartments: [],
+            searchQuery: '',
         };
     },
     mounted() {
         // Fetch apartments sponsored from Laravel API
-        fetch('http://127.0.0.1:8000/api/sponsored-apartments') 
+        fetch('http://127.0.0.1:8000/api/sponsored-apartments')
             .then(response => response.json())
             .then(data => {
-                
+
                 console.log("Dati ricevuti:", data);
                 this.sponsoredApartments = data;
             })
             .catch(error => {
                 console.error("Error fetching sponsored apartments:", error);
             });
+    },
+    // 
+    methods: {
+    goToSearchPage() {
+        this.$router.push({ name: 'search' });
+        }
     }
 };
 </script>
@@ -25,25 +32,26 @@ export default {
     <div>
         <!-- Jumbotron Section -->
         <section class="jumbotron">
-            <div class="container">
-                <h1>LaraBnB</h1>
-                <p>Trova l'appartamento dei tuoi sogni</p>
+            <div class="container jumbotron-content">
+                <h1>LaraBnb</h1>
+                <p>Trova l'appartamento perfetto per il tuo soggiorno</p>
+                <div class="search-button">
+                    <button @click="goToSearchPage">Vai alla pagina di ricerca</button>
+                </div>
             </div>
         </section>
+
 
         <!-- Main Section: Sponsored Apartments -->
         <section class="sponsored-apartments">
             <div class="container">
                 <h2>Appartamenti sponsorizzati</h2>
                 <div class="apartment-list">
-                    <div v-if="sponsoredApartments.length > 0" v-for="apartment in sponsoredApartments"
-                        :key="apartment.id" class="apartment-item">
+                    <div v-for="apartment in sponsoredApartments" :key="apartment.id" class="apartment-item">
                         <h3>{{ apartment.name }}</h3>
                         <p>{{ apartment.description }}</p>
                     </div>
-                    <div v-else>
-                        <p>Nessun appartamento sponsorizzato disponibile.</p>
-                    </div>
+
                 </div>
             </div>
         </section>
@@ -52,9 +60,9 @@ export default {
         <footer class="footer">
             <div class="container">
                 <ul>
-                    <li><a href="#">Chi siamo</a></li>
-                    <li><a href="#">Contatti</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">ESEMPIO</a></li>
+                    <li><a href="#">ESEMPIO</a></li>
+                    <li><a href="#">ESEMPIO</a></li>
                 </ul>
             </div>
         </footer>
@@ -69,6 +77,21 @@ export default {
     text-align: center;
 }
 
+.search-bar {
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.search-bar input {
+    padding: 0.5rem;
+    font-size: 1rem;
+    width: 300px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
 .sponsored-apartments .apartment-list {
     display: flex;
     flex-wrap: wrap;
@@ -81,13 +104,14 @@ export default {
     width: 30%;
 }
 
-
 .footer {
     padding: 2rem;
     background-color: #333;
     color: white;
     text-align: center;
-
+    position: fixed;
+    bottom: 0;
+    width: 100%;
 }
 
 .footer ul {
