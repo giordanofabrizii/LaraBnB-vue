0ac21ea6588ade3c47a6d06c3c8dda38c683be1a
MirkoValle <mirko.valle20@gmail.com>
2024-09-17 10:41:39 +0200

    Added style for serch

--- src/pages/ResearchApp.vue
+++ src/pages/ResearchApp.vue
@@ -152,15 +152,19 @@ export default {
             </div>
         </div>
 
+
+        <section class="apartments">
+            <h2>Consigliati per te</h2>
+            <div class="apartment-list">
+                <RouterLink v-for="apartment in store.apartments" :key="apartment.id" class="apartment-item" :to="{name: 'SingleApartment', params: {slug: apartment.slug}}">
+                    <img :src="'http://127.0.0.1:8000/storage/' + apartment.image " alt="apartment image">
+                    <div class="overlay">
+                        <h4>{{ apartment.name }}</h4>
+                    </div>
+                </RouterLink>
+            </div>
+        </section>
         
-        <ul>
-            <RouterLink v-for="apartment in store.apartments" :key="apartment.id"
-                :to="{ name: 'SingleApartment', params: { slug: apartment.slug } }">
-                <li>
-                    {{ apartment.name }} - {{ apartment.surface }} mq
-                </li>
-            </RouterLink>
-        </ul>
     
         <div v-if="store.apartments.length === 0">
             <h3>Purtroppo non sono presenti appartamenti disponibili</h3>
@@ -259,6 +263,65 @@ a {
     }
 }
 
+
+.apartments{
+
+h2{
+    text-align: center;
+    color: white;
+    padding-top: 2rem;
+}
+
+.apartment-list {
+    padding: 2rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+}
+}
+
+.apartment-item {
+position: relative;
+border: 1px solid #060342;
+border-radius: 24px;
+margin: 1rem;
+box-shadow: rgba(255, 255, 255, 0.3) 0 5vw 6vw -8vw, rgba(255, 255, 255, 0) 0 4.5vw 5vw -6vw, rgba(50, 50, 80, 0.5) 0px 4vw 8vw -2vw, rgba(0, 0, 0, 0.8) 0px 4vw 5vw -3vw;
+overflow: hidden;
+aspect-ratio: 9/12;
+width: 15rem;
+
+img{
+    height: 100%;
+    display: block;
+}
+
+.overlay {
+position: absolute;
+bottom: 0;
+left: 0;
+right: 0;
+background-color: #252626b1;
+overflow: hidden;
+width: 100%;
+height: 0;
+transition: .5s ease;
+display: flex;
+justify-content: center;
+align-items: center;
+
+    h4{
+        color: white;
+        padding: 1rem;
+        text-align: center;
+    }
+}
+
+&:hover .overlay{
+    height: 100%;
+}
+}
 
 
 
